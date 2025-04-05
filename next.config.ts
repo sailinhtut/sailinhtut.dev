706a9a204363b4c2d958b9fc585e66fbfd0e2a35
sailinhtut <sailinhtut76062@gmail.com>
2025-04-05 20:58:59 +0630

    Change config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
-	output: 'export',
+	// output: 'standalone',
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
